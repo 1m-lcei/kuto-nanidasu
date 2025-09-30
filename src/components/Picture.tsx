cf8ea1f169bc0388a2b4fb183bbccb866da10015
1m-lcei <187761927+1m-lcei@users.noreply.github.com>
2025-09-30 19:08:26 +0900

    feat: abandon loading spinner

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 type PictureProps = {
   pathWithoutExtension: string;
   sourceExtension: string;
@@ -7,28 +5,15 @@ type PictureProps = {
 };
 
 function Picture({ pathWithoutExtension, sourceExtension, alt }: PictureProps) {
-  const [isLoading, setIsLoading] = useState(true);
-
   return (
-    <div className="flex justify-center items-center">
-      {isLoading && (
-        // biome-ignore lint/a11y/useSemanticElements: 適切
-        <span
-          className="loading loading-spinner loading-xl"
-          role="status"
-          aria-label="画像を読込中"
-        />
-      )}
-      <picture style={isLoading ? { display: "none" } : {}}>
-        <source srcSet={`${pathWithoutExtension}.avif`} type="image/avif" />
-        <img
-          src={`${pathWithoutExtension}.${sourceExtension}`}
-          alt={alt}
-          className="max-w-full object-contain"
-          onLoad={() => setIsLoading(false)}
-        />
-      </picture>
-    </div>
+    <picture>
+      <source srcSet={`${pathWithoutExtension}.avif`} type="image/avif" />
+      <img
+        src={`${pathWithoutExtension}.${sourceExtension}`}
+        alt={alt}
+        className="max-w-full object-contain"
+      />
+    </picture>
   );
 }
 
