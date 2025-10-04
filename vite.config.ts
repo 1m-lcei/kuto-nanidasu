c20dfd6d490df101279d8cecadd3f810b1d58901
1m-lcei <187761927+1m-lcei@users.noreply.github.com>
2025-10-04 15:48:10 +0900

    chore: prepare deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [tsconfigPaths(), react(), tailwindcss()],
+  base: command === "build" ? "/kuto-nanidasu/" : "/",
   resolve: {
     alias: { "@": "/src" },
   },
@@ -13,4 +14,4 @@ export default defineConfig({
     globals: true,
     setupFiles: "./src/test/setupTest.ts",
   },
-});
+}));
